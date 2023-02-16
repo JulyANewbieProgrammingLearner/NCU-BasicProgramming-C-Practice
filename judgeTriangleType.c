e2915584d0a8242be722dfd1849b36bc85eb1e15
JulyANewbieProgrammingLearner <123918852+JulyANewbieProgrammingLearner@users.noreply.github.com>
2023-02-16 22:55:06 +0800

    Update judgeTriangleType.c

--- judgeTriangleType.c
+++ judgeTriangleType.c
@@ -14,36 +14,37 @@ unsigned int Power(unsigned int base); // hold value
 // start main
 int main(void)
 {
-    printf("The program will help you judge the type of triangle.\n");
+    printf("The program will help you judge the type of triangle.\n"); // intro str
     
-    unsigned int uilength1 = 0;
-    unsigned int uilength2 = 0;
-    unsigned int uilength3 = 0;
+    unsigned int uilength1 = 0; // def and initi variable
+    unsigned int uilength2 = 0; // def and initi variable
+    unsigned int uilength3 = 0; // def and initi variable
 
-    printf("Please enter the length of side of triangle: ");
-    scanf("%d", &uilength1);
-    scanf("%d", &uilength2);
-    scanf("%d", &uilength3);
+    printf("Please enter the length of side of triangle: "); // prompt user enter the num
+    scanf("%d", &uilength1); // read and store the num
+    scanf("%d", &uilength2); // read and store the num
+    scanf("%d", &uilength3); // read and store the num
     
-    while(uilength1 <= 0 || uilength2 <= 0 || uilength3 <= 0)
+    while(uilength1 <= 0 || uilength2 <= 0 || uilength3 <= 0) // avoid the length of triangle to be 0
     {
-        printf("Please enter the length of side of triangle again: ");
-        scanf("%d", &uilength1);
-        scanf("%d", &uilength2);
-        scanf("%d", &uilength3);
+        printf("Please enter the length of side of triangle again: "); // prompt user enter the num
+        scanf("%d", &uilength1); // read and store the num
+        scanf("%d", &uilength2); // read and store the num
+        scanf("%d", &uilength3); // read and store the num
     } // end while
 
-    if(uilength1 == uilength2 && uilength2 == uilength3)
+    // start if
+    if(uilength1 == uilength2 && uilength2 == uilength3) // a = b = c ; judge triangle being equilateral triangle or not
     {
         printf("This is a equilateral triangle.\n");
     }
-    else if(uilength1 != uilength2 && uilength2 != uilength3 && uilength1 != uilength3)
+    else if(uilength1 != uilength2 && uilength2 != uilength3 && uilength1 != uilength3) // a != b && b != c ;three different length of triangle
     {
-        isTriangle_different_Length(uilength1, uilength2, uilength3);
+        isTriangle_different_Length(uilength1, uilength2, uilength3); // right triangle || triangle with different length ( other than right triangle ) || not even an triangle
     }
-    else
+    else // a = b || b = c || c = a
     {
-        isTriangle_TwoEqualLength(uilength1, uilength2, uilength3);
+        isTriangle_TwoEqualLength(uilength1, uilength2, uilength3); // is isosceles triangle or not
     } // end if
 } // end main
 
@@ -120,19 +121,19 @@ void isTriangle_different_Length(unsigned int _uilength1, unsigned int _uilength
     unsigned int uiLength_Minimum =  whichMin(_uilength1, _uilength2, _uilength3);
     unsigned int uiLength_Medium = whichMedium(_uilength1, _uilength2, _uilength3);
     
-    if(uiLength_Minimum + uiLength_Medium > uiLength_Maximum)
+    if(uiLength_Minimum + uiLength_Medium > uiLength_Maximum) // confirm it is an triangle
     {
         // start inner if
-        if( Power(uiLength_Maximum) == Power(uiLength_Minimum) + Power(uiLength_Medium) )
+        if( Power(uiLength_Maximum) == Power(uiLength_Minimum) + Power(uiLength_Medium) ) // the condition of right triangle
         {
             printf("This is an right triangle.\n");
         }
-        else
+        else // a triangle with all different length of sides 
         {
             printf("This is an triangle other than equilateral triangle, right triangle, and isosceles triangle.\n");
         } // end inner if
     }
-    else
+    else // cannot form a triangle
     {
         printf("This is not a triangle.\n");
     }
@@ -142,32 +143,34 @@ void isTriangle_TwoEqualLength(unsigned int _uilength1, unsigned int _uilength2,
 {
     unsigned int uiTwoSide;
     unsigned int uiBottom;
+    
     // start if 
-    if(_uilength1 == _uilength2)
+    if(_uilength1 == _uilength2) // a = b
     {
         uiTwoSide = _uilength1;
         uiBottom = _uilength3;
     }
-    else if(_uilength2 == _uilength3)
+    else if(_uilength2 == _uilength3) // b = c
     {
         uiTwoSide = _uilength2;
         uiBottom = _uilength1;
     }
-    else
+    else // c = a
     {
         uiTwoSide = _uilength1;
         uiBottom = _uilength2;
     } // end if
 
+    // to check the triangle ; smallest + medium > biggest
     unsigned int Triangle_TwoEqualLength_Larger = Bigger(uiTwoSide, uiBottom);
     unsigned int Triangle_TwoEqualLength_smaller = Smaller(uiTwoSide, uiBottom);
 
     // start 2nd if
-    if(Triangle_TwoEqualLength_smaller * 2 > Triangle_TwoEqualLength_Larger)
+    if(Triangle_TwoEqualLength_smaller * 2 > Triangle_TwoEqualLength_Larger) // ensure the triangle exist && is an isosceles triangle
     {
         printf("This is an isosceles triangle.\n");
     }
-    else
+    else // cannot even form a triangle
     {
         printf("This is not a triangle.\n");
     } // end 2nd if
